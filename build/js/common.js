df240f297031b77e182db120d5caacc41658181d
Viktoriia <babenko_v_v@ukr.net>
2016-07-20 17:30:36 +0300

    upd

--- build/js/common.js
+++ build/js/common.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
-
+  $('.members__container').slick({
+	dots: false,
+	arrows: false,
+	infinite: true,
+	autoplay: true,
+	autoplaySpeed: 4000,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	adaptiveHeight: true,
+	fade: true,
+  cssEase: 'linear'
+	});
 
 });
